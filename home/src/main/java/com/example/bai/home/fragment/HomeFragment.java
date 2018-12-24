5d24563036092761dc5f839fe80457cb42021f6b
kangbaibai <kangbaibai@btime.com>
2018-12-24 09:58:52 +0800

    整理

--- home/src/main/java/com/example/bai/home/fragment/HomeFragment.java
+++ home/src/main/java/com/example/bai/home/fragment/HomeFragment.java
@@ -19,5 +19,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, null);
+
     }
 }
